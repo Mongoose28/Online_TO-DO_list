08e58453db098908dd48b75e936757cdab1d876f
Mongoose28 <52271189+Mongoose28@users.noreply.github.com>
2019-07-16 08:15:29 +0600

    Project

--- TODO_list/TODO_list/views.py
+++ TODO_list/TODO_list/views.py
@@ -1,7 +1,56 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from list.models import List
+from .forms import ListForm
+from django.contrib import messages
 
-
+def login(request):
+    return render(request,'login.html')
 
 def home(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all()
+            messages.success(request,("Task Has Been Added To List!"))
+            return render(request,'home.html',{'all_items': all_items})
+
+    else:
+        all_items = List.objects.all()
+        return render(request,'home.html',{'all_items': all_items})
+
+
+def delete(request,list_id):
+    task = List.objects.get(pk=list_id)
+    task.delete()
+    messages.success(request,("Task removed!"))
+    return redirect('home')
+
+def cross_off(request, list_id):
+    task = List.objects.get(pk=list_id)
+    task.completed = True
+    task.save()
+    return redirect('home')
+
+
+def uncross(request, list_id):
+    task = List.objects.get(pk=list_id)
+    task.completed = False
+    task.save()
+    return redirect('home')
+
+
+def edit(request, list_id):
+    if request.method=='POST':
+        task = List.objects.get(pk=list_id)
+        form = ListForm(request.POST or None, instance=task)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request,("Task Has Been Edited!!"))
+            return redirect('home')
+    else:
+        task = List.objects.get(pk=list_id)
+        return render(request,'edit.html',{'task': task})
--- TODO_list/list/models.py
+++ TODO_list/list/models.py
@@ -1,29 +1,9 @@
 from django.db import models
 
-class Registration(models.Model):
-    first_name=models.CharField(max_lentgth=250)
-    Last_name=models.CharField(max_length=250)
-    Phone=models.CharField(max_length=11)
-    Email=models.CharField(max_length=250)
-    Password=models.CharField(max_length=50)
 
+class List(models.Model):
+    task = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
 
-
-
-class User(models.Model):
-    user=models.CharField(max_length=250)
-    password=models.CharField(max_length=200)
-
-
-class Task(models.Model):
-    user_id=models.ForeignKey(User, on_delete=models.CASCADE)
-    Project_name=models.CharField(max_length=300)
-    Status=models.CharField(max_length=10)
-
-class reminder(models.Model):
-    user_id=models.ForeignKey(User,on_delete=models.CASCADE)
-    Date=models.CharField(max_length=120)
-    time=models.CharField(max_length=100)
-
-
-
+    def __str__(self):
+        return self.task + ' | ' + str(self.completed)
